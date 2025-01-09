3cf9a39f99a1bd622e16f379b1a25739936aa9b2
Leong Wei Men <>
2025-01-09 11:53:35 +0800

    -check internet connection error throw by Axios (after attempt to trigger the auth api)

--- App/services/Auth/api.ts
+++ App/services/Auth/api.ts
@@ -3,6 +3,7 @@ import {API_BASE_URL} from '../index';
 import AxiosMockAdapter from 'axios-mock-adapter';
 import {LoginModel} from '../../model/Login';
 import {loginDetails, loginFailed} from '../data/loginData';
+import {showMessage} from 'react-native-flash-message';
 
 /**
  * @function login
@@ -32,13 +33,41 @@ export const login = async (params: LoginModel) => {
       const responseError = await axios.get('/authFailed'); // Request to the mocked endpoint
       console.log(responseError.data.error.message);
 
-      // throw responseError.data.error.message;
       throw new Error(responseError.data.error.message);
     }
   } catch (error) {
     if (error instanceof AxiosError) {
       const errorMessage = error.response?.data?.message || error.message;
-      throw new Error(errorMessage);
+
+      // Check if it's a connection or network-related error
+      if (error.code === 'ECONNABORTED') {
+        showMessage({
+          message: 'Request timeout error!',
+          type: 'danger',
+        });
+      } else if (errorMessage.includes('Network Error')) {
+        showMessage({
+          message: 'Network connection error!',
+          type: 'danger',
+        });
+      } else if (error.response) {
+        // The server responded with an error status
+        showMessage({
+          message: `Response error${error.response}`,
+          type: 'danger',
+        });
+      } else {
+        // Something else happened in setting up the request
+        showMessage({
+          message: `Unknown error${error.message}`,
+          type: 'danger',
+        });
+      }
+    } else {
+      showMessage({
+        message: `Error${error}`,
+        type: 'danger',
+      });
     }
 
     // If error is not an AxiosError, handle it here
